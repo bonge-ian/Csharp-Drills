2d527c8a9b9661040d932fed9cbea5528a6faff4
Bonge <nkaranjah@gmail.com>
2022-03-16 15:35:54 +0300

    Added Configuration Type to Special Model

--- Rainer Stropek Drills/EFHotel/EFHotel.Models/EFHotel.Models/Special.cs
+++ Rainer Stropek Drills/EFHotel/EFHotel.Models/EFHotel.Models/Special.cs
@@ -1,6 +1,9 @@
+using EFHotel.Models.Configuration;
+
 namespace EFHotel.Models;
 
 [Index(nameof(Name), IsUnique = true)]
+[EntityTypeConfiguration(typeof(SpecialConfiguration))]
 public class Special : BaseModel
 {
     [StringLength(100)]
